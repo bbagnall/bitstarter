b847a6a10051e11cdcf7f3551ca723caefb7f4b1
Ubuntu <ubuntu@ip-172-31-38-236.us-west-2.compute.internal>
2013-08-14 04:49:06 +0000

    changed to use buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var contents = fs.readFileSync('index.html');  
+	response.send(contents.toString());
 });
 
 var port = process.env.PORT || 5000;
